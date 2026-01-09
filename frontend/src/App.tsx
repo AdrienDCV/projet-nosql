bb25230b685ab63bba75c5dd643d3b93ce630c60
Adrien DA COSTA VEIGA <adrien.dacostaveiga@jetdev.fr>
2026-01-09 15:33:18 +0100

    ✨ : auto-redirect to home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from "react";
 import './App.css'
 import {HomePage} from "./pages/home/home.page.tsx";
-import {Route, Routes} from "react-router";
+import {Navigate, Route, Routes} from "react-router";
 import {FooterComponent} from "./components/footer/footer.component.tsx";
 import {NavbarComponent} from "./components/navbar/navbar.component.tsx";
 import {BasketPage} from "./pages/basket/basket.page.tsx";
@@ -13,6 +13,7 @@ function App() {
       <NavbarComponent/>
       <Suspense fallback={<h1>Page is loading...</h1>}>
         <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
           <Route index path='/home' element={<HomePage />} />
           <Route index path='/basket' element={<BasketPage />} />
         </Routes>
