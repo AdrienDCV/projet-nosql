b9ba8bba021a980261982ac6d0558ce7dadc29ad
Adrien DA COSTA VEIGA <adrien.dacostaveiga@jetdev.fr>
2026-01-17 22:53:07 +0100

    ✨ : add retrieve products details by id in client-api

--- client-api/src/main/java/com/fisa/clientapi/controllers/ProductController.java
+++ client-api/src/main/java/com/fisa/clientapi/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,6 +44,16 @@ public class ProductController {
 
     return new ResponseEntity<>(
             productDtos,
+            HttpStatus.OK
+    );
+  }
+
+  @GetMapping("/{productId}")
+  public ResponseEntity<ProductDto> retrieveProductById(@PathVariable String productId) {
+    return new ResponseEntity<>(
+            ProductDto.toDto(
+                    productService.retrieveProductById(productId)
+            ),
             HttpStatus.OK
     );
   }
